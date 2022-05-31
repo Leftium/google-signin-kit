42dd54dee11456edb08e2e1952d0359b9f9cc4c6
John-Kim <Leftium@users.noreply.github.com>
2022-05-31 19:39:34 +0900

    Add code from question.
    https://stackoverflow.com/questions/72411045/google-auth-oauth-2-0-sveltekit-wierd-behavior

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte'
+
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+
+    onMount(async () => {
+        const handleCredentialResponse = async (response) => {
+            console.log('Encoded JWT ID token: ' + response.credential);
+            };
+        google.accounts.id.initialize({
+            client_id: clientId,
+            callback: handleCredentialResponse,
+        });
+        google.accounts.id.renderButton(
+            document.getElementById('buttonDiv'),
+            { theme: 'outline', size: 'large' } // customization attributes
+        );
+        google.accounts.id.prompt();
+    });
+
+</script>
+
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+</svelte:head>
+
+<div id=buttonDiv></div>
